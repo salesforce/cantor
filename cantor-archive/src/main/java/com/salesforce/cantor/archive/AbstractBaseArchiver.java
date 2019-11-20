bd32a3ecfa9a01ba015ff8c0c40d5e5f667ca677
Paymon Teyer <pteyer@salesforce.com>
2019-11-20 09:52:32 -0800

    separate classes for archiving different data structures

---
+++ cantor-archive/src/main/java/com/salesforce/cantor/archive/AbstractBaseArchiver.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2019, Salesforce.com, Inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+package com.salesforce.cantor.archive;
+
+import org.apache.commons.compress.archivers.ArchiveInputStream;
+import org.apache.commons.compress.archivers.ArchiveOutputStream;
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
+import org.apache.commons.compress.archivers.tar.TarArchiveOutputStream;
+import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream;
+import org.apache.commons.compress.compressors.gzip.GzipCompressorOutputStream;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static com.salesforce.cantor.common.CommonPreconditions.checkArgument;
+import static com.salesforce.cantor.common.CommonPreconditions.checkString;
+
+abstract class AbstractBaseArchiver {
+
+    static void writeArchiveEntry(final ArchiveOutputStream archive, final String name, final byte[] bytes) throws IOException {
+        final TarArchiveEntry entry = new TarArchiveEntry(name);
+        entry.setSize(bytes.length);
+        archive.putArchiveEntry(entry);
+        archive.write(bytes);
+        archive.closeArchiveEntry();
+    }
+
+    static ArchiveOutputStream getArchiveOutputStream(final Path destination) throws IOException {
+        return new TarArchiveOutputStream(new GzipCompressorOutputStream(new BufferedOutputStream(Files.newOutputStream(destination))));
+    }
+
+    static ArchiveInputStream getArchiveInputStream(final Path archiveFile) throws IOException {
+        return new TarArchiveInputStream(new GzipCompressorInputStream(new BufferedInputStream(Files.newInputStream(archiveFile))));
+    }
+
+    static void checkArchiveArguments(final Object instance, final String namespace, final Path destination) {
+        checkArgument(instance != null, "null cantor instance, can't archive");
+        checkString(namespace, "null/empty namespace, can't archive");
+        checkArgument(Files.notExists(destination), "destination already exists, can't archive");
+    }
+
+    static void checkRestoreArguments(final Object instance, final String namespace, final Path archiveFile) {
+        checkArgument(instance != null, "null objects, can't restore");
+        checkString(namespace, "null/empty namespace, can't restore");
+        checkArgument(Files.exists(archiveFile), "can't locate archive file, can't restore");
+    }
+}
---
+++ cantor-archive/src/main/java/com/salesforce/cantor/archive/EventsArchiver.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2019, Salesforce.com, Inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+package com.salesforce.cantor.archive;
+
+import com.google.protobuf.ByteString;
+import com.salesforce.cantor.Events;
+import org.apache.commons.compress.archivers.ArchiveEntry;
+import org.apache.commons.compress.archivers.ArchiveInputStream;
+import org.apache.commons.compress.archivers.ArchiveOutputStream;
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static com.salesforce.cantor.common.CommonPreconditions.checkArgument;
+
+public class EventsArchiver extends AbstractBaseArchiver {
+    private static final Logger logger = LoggerFactory.getLogger(EventsArchiver.class);
+
+    public static final long MIN_ALLOWED_CHUNK_MILLIS = TimeUnit.MINUTES.toMillis(1);
+    public static final long MAX_ALLOWED_CHUNK_MILLIS = TimeUnit.MINUTES.toMillis(60);
+
+    public static void archive(final Events events, final String namespace, final long startTimestampMillis,
+                               final long endTimestampMillis, final long chunkMillis, final Path destination) throws IOException {
+        checkArchiveArguments(events, namespace, destination);
+        checkArgument(startTimestampMillis > 0, "start timestamp must be positive");
+        checkArgument(startTimestampMillis < endTimestampMillis, "start timestamp must be before end timestamp");
+        checkArgument(chunkMillis >= MIN_ALLOWED_CHUNK_MILLIS, "archive chunk millis must be greater than " + MIN_ALLOWED_CHUNK_MILLIS);
+        checkArgument(chunkMillis <= MAX_ALLOWED_CHUNK_MILLIS, "archive chunk millis must be less than " + MAX_ALLOWED_CHUNK_MILLIS);
+
+        try (final ArchiveOutputStream archive = getArchiveOutputStream(destination)) {
+            long chunkStart = startTimestampMillis;
+            long chunkEnd = Math.min(chunkStart + chunkMillis, endTimestampMillis);
+            while (chunkStart < endTimestampMillis) {
+                final String name = String.format("events-%s-%s-%s", namespace, chunkStart, chunkEnd);
+                final List<Events.Event> chunkEvents = events.get(namespace, chunkStart, chunkEnd, true);
+                // todo: can we do this differently? This doubles the memory we hold on to :(
+                final EventsChunk.Builder chunkBuilder = EventsChunk.newBuilder();
+                for (final Events.Event event : chunkEvents) {
+                    final EventsChunk.Event.Builder eventBuilder = EventsChunk.Event.newBuilder()
+                            .setTimestampMillis(event.getTimestampMillis())
+                            .putAllDimensions(event.getDimensions())
+                            .putAllMetadata(event.getMetadata());
+                    if (event.getPayload() != null && event.getPayload().length > 0) {
+                        eventBuilder.setPayload(ByteString.copyFrom(event.getPayload()));
+                    }
+                    chunkBuilder.addEvents(eventBuilder.build());
+                }
+                writeArchiveEntry(archive, name, chunkBuilder.build().toByteArray());
+                chunkStart = chunkEnd;
+                chunkEnd = Math.min(chunkStart + chunkMillis, endTimestampMillis);
+            }
+        }
+    }
+    public static void restore(final Events events, final String namespace, final Path archiveFile) throws IOException {
+        checkRestoreArguments(events, namespace, archiveFile);
+        // create the namespace, in case the user hasn't already
+        events.create(namespace);
+        try (final ArchiveInputStream archive = getArchiveInputStream(archiveFile)) {
+            ArchiveEntry entry;
+            int total = 0;
+            while ((entry = archive.getNextEntry()) != null) {
+                final EventsChunk chunk = EventsChunk.parseFrom(archive);
+                for (final EventsChunk.Event event : chunk.getEventsList()) {
+                    if (ByteString.EMPTY.equals(event.getPayload())) {
+                        events.store(namespace, event.getTimestampMillis(), event.getMetadataMap(), event.getDimensionsMap());
+                    } else {
+                        events.store(namespace, event.getTimestampMillis(), event.getMetadataMap(), event.getDimensionsMap(), event.getPayload().toByteArray());
+                    }
+                }
+                logger.info("read {} entries from chunk {} ({} bytes) into {}", chunk.getEventsCount(), entry.getName(), entry.getSize(), namespace);
+                total += chunk.getEventsCount();
+            }
+            logger.info("restored {} events into namespace '{}' from archive file {}", total, namespace, archiveFile);
+        }
+    }
+}
