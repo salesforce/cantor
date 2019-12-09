a70f89b80a6cdd0dde9952446ddadab44eee7c93
Paul Howden <pmhowden@gmail.com>
2019-12-09 12:57:21 -0800

    Clean up metrics

--- cantor-metrics/src/main/java/com/salesforce/cantor/metrics/MetricCollectingEvents.java
+++ cantor-metrics/src/main/java/com/salesforce/cantor/metrics/MetricCollectingEvents.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2019, Salesforce.com, Inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
 package com.salesforce.cantor.metrics;
 
 import com.codahale.metrics.MetricRegistry;
--- cantor-metrics/src/test/java/com/salesforce/cantor/metrics/MetricCollectingObjectsTest.java
+++ cantor-metrics/src/test/java/com/salesforce/cantor/metrics/MetricCollectingObjectsTest.java
@@ -1,11 +1,16 @@
+/*
+ * Copyright (c) 2019, Salesforce.com, Inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
 package com.salesforce.cantor.metrics;
 
 import com.salesforce.cantor.Cantor;
 import com.salesforce.cantor.common.AbstractBaseObjectsTest;
 
 import java.io.IOException;
-
-import static org.testng.Assert.*;
 
 public class MetricCollectingObjectsTest extends AbstractBaseObjectsTest {
 
