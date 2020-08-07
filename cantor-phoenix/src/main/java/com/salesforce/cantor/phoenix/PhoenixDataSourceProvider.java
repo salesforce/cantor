dc827a2b6e523880b0dc932c3fe6146876706ede
Yuhan Xiao <yxiao@salesforce.com>
2020-08-06 17:42:09 -0700

    removed unnecessary data source properties

--- cantor-phoenix/src/main/java/com/salesforce/cantor/phoenix/PhoenixDataSourceProvider.java
+++ cantor-phoenix/src/main/java/com/salesforce/cantor/phoenix/PhoenixDataSourceProvider.java
@@ -10,11 +10,11 @@ public class PhoenixDataSourceProvider {
     private static final Logger logger = LoggerFactory.getLogger(PhoenixDataSourceProvider.class);
 
     public static synchronized DataSource getDatasource(final PhoenixDataSourceProperties builder) {
-        return doGetDataSource();
+        return doGetDataSource(builder);
     }
 
-    private static DataSource doGetDataSource() {
-        final String jdbcUrl = "jdbc:phoenix:akouthoofd-wsl";
+    private static DataSource doGetDataSource(final PhoenixDataSourceProperties builder) {
+        final String jdbcUrl = "jdbc:phoenix:" + builder.getHostname() + ":" + builder.getPath();
         try {
             // force loading Phoenix drivers
             Class.forName("org.apache.phoenix.jdbc.PhoenixDriver");
@@ -23,35 +23,7 @@ public class PhoenixDataSourceProvider {
         }
 
         final HikariDataSource connectionPoolDataSource = new HikariDataSource();
-
         connectionPoolDataSource.setJdbcUrl(jdbcUrl);
-
-        connectionPoolDataSource.setConnectionTimeout(30 * 1000);  // 30 seconds connection timeout
-        connectionPoolDataSource.addDataSourceProperty("useUnicode", true);
-        connectionPoolDataSource.addDataSourceProperty("useSSL", false);
-        connectionPoolDataSource.addDataSourceProperty("allowPublicKeyRetrieval", true);
-        connectionPoolDataSource.addDataSourceProperty("serverTimezone", "UTC");
-        connectionPoolDataSource.addDataSourceProperty("useJDBCCompliantTimezoneShift", true);
-        connectionPoolDataSource.addDataSourceProperty("useLegacyDatetimeCode", false);
-        connectionPoolDataSource.addDataSourceProperty("cachePrepStmts", true);
-        connectionPoolDataSource.addDataSourceProperty("prepStmtCacheSize", 512);
-        connectionPoolDataSource.addDataSourceProperty("prepStmtCacheSqlLimit", 2048);
-        connectionPoolDataSource.addDataSourceProperty("useServerPrepStmts", true);
-        connectionPoolDataSource.addDataSourceProperty("useLocalSessionState", true);
-        connectionPoolDataSource.addDataSourceProperty("rewriteBatchedStatements", true);
-        connectionPoolDataSource.addDataSourceProperty("cacheResultSetMetadata", true);
-        connectionPoolDataSource.addDataSourceProperty("metadataCacheSize", 512);
-        connectionPoolDataSource.addDataSourceProperty("cacheServerConfiguration", true);
-        connectionPoolDataSource.addDataSourceProperty("elideSetAutoCommits", true);
-        connectionPoolDataSource.addDataSourceProperty("continueBatchOnError", false);
-        connectionPoolDataSource.addDataSourceProperty("maintainTimeStats", false);
-        connectionPoolDataSource.addDataSourceProperty("maxRows", 100_000);  // max of 100,000 rows to be returned
-//        TODO allow users to override this by a flag
-//        connectionPoolDataSource.addDataSourceProperty("logger", "com.mysql.cj.log.Slf4JLogger");
-//        connectionPoolDataSource.addDataSourceProperty("profileSQL", true);
-
-        connectionPoolDataSource.setMaximumPoolSize(64);
-
         logger.info("jdbc url for datasource is: {} data source properties are: {}",
                 jdbcUrl, connectionPoolDataSource.getDataSourceProperties()
         );
