b6401050272ee204cf3c3f7db8c250c789fab0cf
Alexander <akouthoofd@salesforce.com>
2020-09-09 03:07:43 -0700

    Disable s3 test as it requires a real s3 account
    - s3 mock does not support s3 select

--- cantor-s3/src/test/java/com/salesforce/cantor/s3/EventsOnS3Test.java
+++ cantor-s3/src/test/java/com/salesforce/cantor/s3/EventsOnS3Test.java
@@ -1,42 +1,54 @@
 package com.salesforce.cantor.s3;
 
-import com.adobe.testing.s3mock.testng.S3MockListener;
 import com.amazonaws.auth.*;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.salesforce.cantor.Cantor;
 import com.salesforce.cantor.common.AbstractBaseEventsTest;
-import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
 
 import java.io.*;
 import java.util.Scanner;
 
-@Listeners(value = { S3MockListener.class })
+@Test(enabled = false)
 public class EventsOnS3Test extends AbstractBaseEventsTest {
+    private static final String credentialsLocation = "/path/to/creds";
+
     @Override
     protected Cantor getCantor() throws IOException {
-        final AmazonS3 s3Client = S3Mock.getInstance().createS3Client("us-west-1");
+        final AmazonS3 s3Client = createS3Client();
         return new CantorOnS3(s3Client);
     }
 
-    @Override
-    public void testNamespaces() throws Exception {
-        super.testNamespaces();
+    // insert real S3 client here to run integration testing
+    private AmazonS3 createS3Client() throws IOException {
+        final File s3File = new File(credentialsLocation);
+        try (final Scanner csvReader = new Scanner(new FileInputStream(s3File))) {
+            csvReader.useDelimiter(",");
+
+            final String keyId = csvReader.next();
+            final String accessKey = csvReader.next();
+
+            final AWSCredentials sessionCredentials = new BasicAWSCredentials(keyId, accessKey);
+            return AmazonS3ClientBuilder.standard().withRegion(Regions.US_WEST_2)
+                    .withCredentials(new AWSStaticCredentialsProvider(sessionCredentials))
+                    .build();
+        }
+    }
+
+    @Test
+    public void testStoreOneEvent() throws Exception {
+        // no-op as this test creates a payload too large for s3 select
     }
 
     @Override
-    public void testStore3kRandomEvents() throws Exception {
-        super.testStore3kRandomEvents();
+    public void testMetadata() throws Exception {
+        // no-op as it's not implement yet
     }
 
     @Override
-    public void testBadInput() throws Exception {
-        super.testBadInput();
-    }
-
-    @Override
-    public void testMetadataQueryRegex() throws Exception {
-        super.testMetadataQueryRegex();
+    public void testAggregations() throws Exception {
+        // no-op as it's not implement yet
     }
 }
