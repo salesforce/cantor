029cdef3183d07b34f7dff4bcae75a48ee2e973c
Yuhan Xiao <yuhan.xiao@salesforce.com>
2021-03-29 13:42:58 -0700

    create new module

---
+++ cantor-grpc-client-sfdc/src/main/java/com/salesforce/cantor/grpc/CantorOnGrpc.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2020, Salesforce.com, Inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+package com.salesforce.cantor.grpc;
+
+import com.salesforce.cantor.*;
+
+public class CantorOnGrpc implements Cantor {
+    private final Objects objects;
+    private final Sets sets;
+    private final Events events;
+
+    public CantorOnGrpc(final String target) {
+        this.objects = new ObjectsOnGrpc(target);
+        this.sets = new SetsOnGrpc(target);
+        this.events = new EventsOnGrpc(target);
+    }
+
+    @Override
+    public Objects objects() {
+        return this.objects;
+    }
+
+    @Override
+    public Sets sets() {
+        return this.sets;
+    }
+
+    @Override
+    public Events events() {
+        return this.events;
+    }
+}
---
+++ cantor-grpc-client-sfdc/src/main/java/com/salesforce/cantor/grpc/EventsOnGrpc.java
@@ -0,0 +1,162 @@
+/*
+ * Copyright (c) 2020, Salesforce.com, Inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+package com.salesforce.cantor.grpc;
+
+import com.salesforce.servicelibs.srpc.grpc.com.google.protobuf.ByteString;
+import com.salesforce.cantor.Events;
+import com.salesforce.cantor.grpc.events.*;
+import com.salesforce.cantor.grpc.events.EventsServiceGrpc.EventsServiceBlockingStub;
+
+import java.io.IOException;
+import java.util.*;
+
+import static com.salesforce.cantor.common.CommonUtils.nullToEmpty;
+import static com.salesforce.cantor.common.EventsPreconditions.*;
+
+public class EventsOnGrpc extends AbstractBaseGrpcClient<EventsServiceBlockingStub> implements Events {
+
+    public EventsOnGrpc(final String target) {
+        super(EventsServiceGrpc::newBlockingStub, target);
+    }
+
+    @Override
+    public Collection<String> namespaces() throws IOException {
+        return call(() -> {
+            final NamespacesRequest request = NamespacesRequest.newBuilder().build();
+            return getStub().namespaces(request).getNamespacesList();
+        });
+    }
+
+    @Override
+    public void create(final String namespace) throws IOException {
+        checkCreate(namespace);
+        call(() -> {
+            final CreateRequest request = CreateRequest.newBuilder()
+                    .setNamespace(namespace)
+                    .build();
+            getStub().create(request);
+            return null;
+        });
+    }
+
+    @Override
+    public void drop(final String namespace) throws IOException {
+        checkDrop(namespace);
+        call(() -> {
+            final DropRequest request = DropRequest.newBuilder()
+                    .setNamespace(namespace)
+                    .build();
+            getStub().drop(request);
+            return null;
+        });
+    }
+
+    @Override
+    public void store(final String namespace, final Collection<Event> batch) throws IOException {
+        checkStore(namespace, batch);
+        call(() -> {
+            final List<EventProto> eventBatch = new ArrayList<>();
+            for (final Event event : batch) {
+                eventBatch.add(EventProto.newBuilder()
+                        .setTimestampMillis(event.getTimestampMillis())
+                        .putAllMetadata(event.getMetadata())
+                        .putAllDimensions(event.getDimensions())
+                        .setPayload(event.getPayload() != null ? ByteString.copyFrom(event.getPayload()) : ByteString.EMPTY)
+                        .build()
+                );
+            }
+            final StoreRequest request = StoreRequest.newBuilder()
+                    .setNamespace(namespace)
+                    .addAllBatch(eventBatch)
+                    .build();
+            getStub().store(request);
+            return null;
+        });
+    }
+
+    @Override
+    public List<Event> get(final String namespace,
+                           final long startTimestampMillis,
+                           final long endTimestampMillis,
+                           final Map<String, String> metadataQuery,
+                           final Map<String, String> dimensionsQuery,
+                           final boolean includePayloads,
+                           final boolean ascending,
+                           final int limit) throws IOException {
+        checkGet(namespace, startTimestampMillis, endTimestampMillis, metadataQuery, dimensionsQuery);
+        return call(() -> {
+            final GetRequest request = GetRequest.newBuilder()
+                    .setNamespace(namespace)
+                    .setStartTimestampMillis(startTimestampMillis)
+                    .setEndTimestampMillis(endTimestampMillis)
+                    .putAllMetadataQuery(nullToEmpty(metadataQuery))
+                    .putAllDimensionsQuery(nullToEmpty(dimensionsQuery))
+                    .setIncludePayloads(includePayloads)
+                    .setAscending(ascending)
+                    .setLimit(limit)
+                    .build();
+            final List<Event> results = new ArrayList<>();
+            final GetResponse response = getStub().get(request);
+            final List<EventProto> eventProtos = response.getResultsList();
+            for (final EventProto proto : eventProtos) {
+                final ByteString payloadByteString = proto.getPayload();
+                results.add(
+                        new Event(
+                                proto.getTimestampMillis(),
+                                proto.getMetadataMap(),
+                                proto.getDimensionsMap(),
+                                payloadByteString != null ? payloadByteString.toByteArray() : null
+                        )
+                );
+            }
+            return results;
+        });
+    }
+
+    @Override
+    public Set<String> metadata(final String namespace,
+                                final String metadataKey,
+                                final long startTimestampMillis,
+                                final long endTimestampMillis,
+                                final Map<String, String> metadataQuery,
+                                final Map<String, String> dimensionsQuery) throws IOException {
+        checkMetadata(namespace,
+                metadataKey,
+                startTimestampMillis,
+                endTimestampMillis,
+                metadataQuery,
+                dimensionsQuery
+        );
+        return call(() -> {
+            final MetadataRequest request = MetadataRequest.newBuilder()
+                    .setNamespace(namespace)
+                    .setMetadataKey(metadataKey)
+                    .setStartTimestampMillis(startTimestampMillis)
+                    .setEndTimestampMillis(endTimestampMillis)
+                    .putAllMetadataQuery(nullToEmpty(metadataQuery))
+                    .putAllDimensionsQuery(nullToEmpty(dimensionsQuery))
+                    .build();
+            final MetadataResponse metadataResponse = getStub().metadata(request);
+            return new HashSet<>(metadataResponse.getValuesList());
+        });
+    }
+
+    @Override
+    public void expire(final String namespace, final long endTimestampMillis) throws IOException {
+        checkExpire(namespace, endTimestampMillis);
+        call(() -> {
+            final ExpireRequest request = ExpireRequest.newBuilder()
+                    .setNamespace(namespace)
+                    .setEndTimestampMillis(endTimestampMillis)
+                    .build();
+            getStub().expire(request);
+            return null;
+        });
+    }
+}
+
